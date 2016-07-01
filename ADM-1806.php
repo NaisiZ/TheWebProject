27338cf46dc959704f594e4b15fce559066b1521
NaisiZ <zhennaisi@gmail.com>
2016-06-30 22:58:53 -0700

    Add files via upload

---
+++ ADM-1806.php
@@ -0,0 +1,24 @@
+<table class="table table-condensed table-striped">
+<tbody>
+<tr>
+<td style="background-color: #bebec2;"><span style="background-color: #c0c0c0;"><strong><span style="color: #8a111d;"><span class="header_red_11pt_geneva">&nbsp;Dimensional Drawing<br /></span></span></strong></span></td>
+<td style="background-color: #bebec2;"><span style="background-color: #c0c0c0;"><strong><span style="color: #8a111d;"><span class="header_red_11pt_geneva">&nbsp;VESA Mount Bracket (optional) <span style="color: #000000;">Ordering Code: VESA-3500</span><br /></span></span></strong></span></td>
+</tr>
+<tr>
+<td style="width: 50%;"><img src="http://goarista.us/media/wysiwyg/AP3500-3.jpg" alt="AP-3500" /></td>
+<td style="width: 50%;"><img src="http://goarista.us/media/wysiwyg/AP3500-4.jpg" alt="AP-3500" /></td>
+</tr>
+<tr>
+<td style="width: 50%;"><img src="http://goarista.us/media/wysiwyg/AP3500-1.jpg" alt="AP-3500" /></td>
+<td style="width: 50%;"><img src="http://goarista.us/media/wysiwyg/AP3500-2.jpg" alt="AP-3500" /></td>
+</tr>
+<tr>
+<td style="background-color: #bebec2;"><span style="background-color: #c0c0c0;"><strong><span style="color: #8a111d;"><span class="header_red_11pt_geneva">&nbsp;Power Adaptor Mounting Bracket <span style="color: #000000;">Ordering Code: APM-001</span><br /></span></span></strong></span></td>
+<td style="background-color: #bebec2;"></td>
+</tr>
+<tr>
+<td style="width: 50%;"><img src="http://goarista.us/media/wysiwyg/AP3500-5.jpg" alt="AP-3500" /></td>
+<td style="width: 50%;"><img src="http://goarista.us/media/wysiwyg/AP3500-6.jpg" alt="AP-3500" /></td>
+</tr>
+</tbody>
+</table>
